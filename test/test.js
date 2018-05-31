c6ea91dac4159696115168bc5e40139700d9e49f
Josue <victorjosue1260@gmail.com>
2018-05-31 12:55:36 -0600

    se agrega como hacer la documentacion

--- test/test.js
+++ test/test.js
@@ -3,23 +3,20 @@ var selenium = require('selenium-webdriver');
 var firefox = require('selenium-webdriver/firefox');
 var geckodriver = require('geckodriver');
 const chromedriver = require('chromedriver');
+const addContext = require('mochawesome/addContext');
 
 var driver;
-
-async function iniciarSecion(controlador){
-    await controlador.findElement(selenium.By.id('inputUsuario')).sendKeys('erick');
-    await controlador.findElement(selenium.By.id('inputContraseña')).sendKeys('12345');
-    await controlador.findElement(selenium.By.id('botonSubmit')).click();
-}
+var x=0;
 
 describe('Pruebas login', () => {
 
     beforeEach(async function() {
         this.timeout(25000);
+        x++;
         driver = new selenium.Builder()
             .withCapabilities(selenium.Capabilities.chrome())
             .build();
-        await driver.get("http://localhost:63342/Proyecto-Trasportes/index.html");
+        await driver.get("http://172.24.4.41/Transportec/");
     });
 
     afterEach(() => {
@@ -28,6 +25,15 @@ describe('Pruebas login', () => {
 
     it('Sin datos', async function() {
         this.timeout(15000);
+
+        addContext(this, 'Caso de prueba: Inicio de session sin datos' );
+        addContext(this, "Identificador: Cp"+x);
+        addContext(this, "Funcion a probar: login");
+        addContext(this,"Objetivo: verificar que no se pueda ingresar sin datos en la aplicacion");
+        addContext(this,"Descripcion: se inteta iniciar sesion sin ningun dato en los campos requeridos");
+        addContext(this,"Precondiciones: que el servidor este arriba y estar conectado a la misma red del servidor");
+        addContext(this,"Criterios de aceptacion: mensaje de error que muestre que hacen falta los campos");
+
         await driver.findElement(selenium.By.id('botonSubmit')).click();
 
         var message = await driver.findElement(selenium.By.xpath('//*[@id="loginBody"]/div[4]/p')).getText();
@@ -38,6 +44,15 @@ describe('Pruebas login', () => {
     it('Sin contraseña', async function() {
         this.timeout(15000);
 
+
+        addContext(this, 'Caso de prueba: Inicio de session sin contraseña' );
+        addContext(this, "Identificador: Cp"+x);
+        addContext(this, "Funcion a probar: login");
+        addContext(this,"Objetivo: verificar que no se pueda ingresar sin contraseña en la aplicacion");
+        addContext(this,"Descripcion: se inteta iniciar sesion sin el dato contraseña en el campos requerido");
+        addContext(this,"Precondiciones: que el servidor este arriba y estar conectado a la misma red del servidor");
+        addContext(this,'Criterios de aceptacion: mensaje de error "usuario o contraseña incorrecta"');
+
         await driver.findElement(selenium.By.id('inputUsuario')).sendKeys('erick');
         await driver.findElement(selenium.By.id('botonSubmit')).click();
         var message = await driver.findElement(selenium.By.xpath('//*[@id="loginBody"]/div[4]/p')).getText();
@@ -47,6 +62,14 @@ describe('Pruebas login', () => {
     it('Contraseña Incorrecta', async function() {
         this.timeout(15000);
 
+        addContext(this, 'Caso de prueba: Inicio de session con contraseña incorrecta' );
+        addContext(this, "Identificador: Cp"+x);
+        addContext(this, "Funcion a probar: login");
+        addContext(this,"Objetivo: verificar que no se pueda ingresar con una contraseña incorrecta");
+        addContext(this,"Descripcion: se inteta iniciar sesion con una contraseña que no corresponde al usuario");
+        addContext(this,"Precondiciones: que el servidor este arriba y estar conectado a la misma red del servidor");
+        addContext(this,'Criterios de aceptacion: mensaje de error "usuario o contraseña incorrecta"');
+
         await driver.findElement(selenium.By.id('inputUsuario')).sendKeys('erick');
         await driver.findElement(selenium.By.id('inputContraseña')).sendKeys('12');
         await driver.findElement(selenium.By.id('botonSubmit')).click();
@@ -54,24 +77,5 @@ describe('Pruebas login', () => {
         var message =  await driver.findElement(selenium.By.xpath('//*[@id="loginBody"]/div[4]/p')).getText();
         assert.equal(message,'Nombre de usuario o contraseña incorrectas.');
     });
-});
-
-describe('Pruebas caja negra', () => {
-    beforeEach(async function() {
-        this.timeout(25000);
-        driver = new selenium.Builder().forBrowser('firefox').build();
-        await driver.get("http://localhost:63342/Proyecto-Trasportes/users/#/user");
-    });
-
-    afterEach(() => {
-        driver.quit();
-    });
-
-    it('prueba',async function () {
-        await driver.findElement(selenium.By.xpath('//*[@id="reservationTab"]/a')).click();
-        await driver.findElement(selenium.By.xpath('//*[@id="departureHour"]'));
-
-    });
-    
 });
 
